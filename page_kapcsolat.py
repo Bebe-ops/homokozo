2fab833ccf7e8a961c1bf4ef35681424a385fc32
Bernadett Szokol <animalsavebebe@gmail.com>
2022-03-28 11:57:19 +0200

    added requirements.txt

--- page_kapcsolat.py
+++ page_kapcsolat.py
@@ -6,15 +6,15 @@ from selenium.webdriver.common.by import By
 
 
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 # options.add_argument('--disable-gpu')
 
 
 # Homepage-form
 class ContactForm:
     def __init__(self):
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        # self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        # self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         self.driver.get('https://sandbox.develop.y-collective.hu/kapcsolat/')
         self.driver.maximize_window()
         self.input_fields_tag_name = 'input'
